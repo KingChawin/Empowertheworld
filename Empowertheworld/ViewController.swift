958ebefa84f25850a3d11465c0e8e8ce6a49a92c
Chris S <Savanapridi@Chriss-MacBook-Air.local>
2017-02-08 22:56:05 -0800

    Initial Commit

---
+++ Empowertheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Empowertheworld
+//
+//  Created by Chris S on 2/8/17.
+//  Copyright © 2017 Chris S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
